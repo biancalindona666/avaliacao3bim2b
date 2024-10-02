8965b969eab63907a1287e7bb2ddf02251732aac
Bianca a mais mais <00001094322994al@educacao.sp.gov.br>
2024-10-01 21:03:45 -0300

    biancalinda

---
+++ bianca.py
@@ -0,0 +1,6 @@
+candidatos={
+257:{"Higor chato": "lgbt"},
+244:{"Biancalinda": "prft"}
+}
+numero = int(input("digite o numero do cantitato: "))
+if numero in cantidatos : git config --global user.name "	Seu nome"
